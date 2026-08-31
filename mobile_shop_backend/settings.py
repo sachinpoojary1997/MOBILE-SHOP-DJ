8bbae32c0ef786fc5c0bb6de1498f5f8c1f831d6
sachin poojary <sachikrishna1997@gmail.com>
2026-08-31 17:44:01 +0530

    data base changed to sqlite to postgres

--- mobile_shop_backend/settings.py
+++ mobile_shop_backend/settings.py
@@ -86,13 +86,23 @@ WSGI_APPLICATION = 'mobile_shop_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mobile_shop_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres@17',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
